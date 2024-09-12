14c77316354e1fec459a751dcd7b7f2b4b4a544a
Velin Dankov <velin80@web.de>
2024-09-12 15:42:18 +0300

    route to html and creating cats.json file register static files and dynamic index to show all cats

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+
+const { staticFileHandler } = require("./src/handlers/static");
+const { homeHandler } = require("./src/handlers/home");
+
+const routes = {
+  "/": homeHandler,
+  "index.html": homeHandler
+};
+
+http
+  .createServer((req, res) => {
+    const route = routes[req.url];
+    if (typeof route === "function") {
+      route(req, res);
+      return;
+    } else if (staticFileHandler(req, res)) {
+      return;
+    } 
+    res.writeHead(404, ["Content-Type", "text/plaint"]);
+    res.write("404 Not Found");
+    res.end();
+  })
+  .listen(3000);
